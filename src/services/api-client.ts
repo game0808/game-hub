6935655165305f903834669f439a3d5c22d338ce
Administrator <cchau33@gwmail.gwu.edu>
2023-07-09 23:18:04 -0400

    Build game cards

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "e8ac9ce289a64facb480e6261e60ec15",
+    key: "5515016228254ce5b81dc547b9c95026",
   },
 });
